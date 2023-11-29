074da232f6e9a7f3b35a2cb171992dab2006bbd4
salimovrashad <selimovresad33@gmail.com>
2023-11-29 12:49:42 +0400

    add admin panel

---
+++ Employee/Employee/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Employee.Helpers;
+using Employee.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace Employee.Controllers
+{
+	public class HomeController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			var dt = await SqlHelper.GetDatas("SELECT * FROM Employees");
+			return View(dt);
+		}
+		public IActionResult Contact()
+		{
+			return View();
+		}
+	}
+}
